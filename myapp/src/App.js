3ef1f4a147754e6d99fb11225d29af2204780a13
rjingithub321 <34452286+rjingithub321@users.noreply.github.com>
2017-12-17 20:17:40 +0530

    Update App.js

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -5,8 +5,6 @@ import TabsExampleIconText from './Part1';
 import PaperExampleSimple1 from './Part2';
 import PaperExampleSimple2 from './Part3';
 import PaperExampleSimple3 from './Part4';
-import PaperExampleSimple4 from './Part5';
-import PaperExampleSimple5 from './Part6';
 import {lightBlue100, lightBlue500, lightBlue700} from 'material-ui/styles/colors';
 
 
@@ -21,11 +19,9 @@ const muiTheme = getMuiTheme({
 const App = () => (
   <MuiThemeProvider muiTheme={muiTheme}>
     <TabsExampleIconText/>
-    <div style={{ float: 'left' }}><PaperExampleSimple1/></div>
-    <div style={{ float: 'left' }}><PaperExampleSimple2/></div>
+    <PaperExampleSimple1/>
+    <PaperExampleSimple2/>
     <PaperExampleSimple3/>
-    <PaperExampleSimple4/>
-    <PaperExampleSimple5/>
   </MuiThemeProvider>
 );
 export default App;
